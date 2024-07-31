255b22cb7446d033be2f8eb9ecb047f7b5609e73
Hải Đăng <haidang02032003@gmail.com>
2024-07-31 16:05:20 +0700

    :bug: base backend

--- src/app.middleware.ts
+++ src/app.middleware.ts
@@ -3,8 +3,8 @@ import passport from 'passport';
 import compression from "compression";
 
 export function middleware(app: INestApplication): INestApplication {
-  app.use(compression());
-  app.use(passport.initialize());
+  // app.use(compression());
+  // app.use(passport.initialize());
   app.enableCors();
   return app;
 }
