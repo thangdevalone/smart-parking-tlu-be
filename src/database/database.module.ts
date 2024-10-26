9e17a6931beea2323b7131ebfd5894244ce41ed3
haidaqn <haidang02032003@gmail.com>
2024-10-26 16:38:20 +0700

    docker

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,17 +7,19 @@ import { Bill, Card, CardType, History, Payment, Role, User } from '../modules';
   imports: [
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('db.host'),
-        port: +configService.get<number>('db.port_db'),
-        username: configService.get<string>('db.namedb'),
-        password: configService.get<string>('db.password'),
-        database: configService.get<string>('db.database'),
-        entities: [User, Card, CardType, Bill, History, Role, Payment],
-        synchronize: true,
-        driver: require('mysql2')
-      }),
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: configService.get<string>('db.host'),
+          port: +configService.get<number>('db.port_db'),
+          username: configService.get<string>('db.namedb'),
+          password: configService.get<string>('db.password'),
+          database: configService.get<string>('db.database'),
+          entities: [User, Card, CardType, Bill, History, Role, Payment],
+          synchronize: true,
+          driver: require('mysql2')
+        }
+      },
       inject: [ConfigService]
     })
   ]
